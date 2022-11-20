a0c13adb47f9136929bd713f12a4aa06d9b66cc4
Julius Dsouza <jsdsz1996@gmail.com>
2022-11-20 18:48:58 -0500

    updated topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -12,12 +12,12 @@ const Topbar = () => {
   const [activeNav, setActiveNav] = useState('#home');
   return (
     <nav>
-      <Link to="home" spy={true} smooth={true} duration={500} onClick={()=> setActiveNav('#home')} ><AiOutlineHome /></Link>
-      <Link to="about" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#about')} ><AiOutlineUser /></Link>
-      <Link to="experience" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#experience')} ><BiBook /></Link>
-      <Link to="timeline" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#timeline')} ><CiViewTimeline /></Link>
-      <Link to="portfolio" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#portfolio')} ><FaBusinessTime /></Link>
-      <Link to="contact" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#contact')} ><MdOutlineContactMail /></Link>
+      <Link to="home" spy={true} smooth={true} duration={500} onClick={()=> setActiveNav('#home')} className={activeNav === "#home" ? "active" : ""} ><AiOutlineHome /></Link>
+      <Link to="about" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#about')} className={activeNav === "#about" ? "active" : ""} ><AiOutlineUser /></Link>
+      <Link to="experience" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#experience')} className={activeNav === "#experience" ? "active" : ""} ><BiBook /></Link>
+      <Link to="timeline" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#timeline')} className={activeNav === "#timeline" ? "active" : ""} ><CiViewTimeline /></Link>
+      <Link to="portfolio" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#portfolio')} className={activeNav === "#portfolio" ? "active" : ""} ><FaBusinessTime /></Link>
+      <Link to="contact" spy={true} smooth={true} offset={-50} duration={500} onClick={()=> setActiveNav('#contact')} className={activeNav === "#contact" ? "active" : ""} ><MdOutlineContactMail /></Link>
     </nav>
   )
 }
