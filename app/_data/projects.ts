8bab6ffbeb8a938f7d29b2b06c57c350311f9a6b
Julius Dsouza <jsdsz1996@gmail.com>
2024-12-20 05:00:22 -0800

    Added Projects Page

---
+++ app/_data/projects.ts
@@ -0,0 +1,46 @@
+export const WEB_APPS = [
+  {
+    title: "AI Generated Images Gallery",
+    tags: [
+      "React",
+      "JavaScript",
+      "Tailwind CSS",
+      "MongoDB",
+      "Express.js",
+      "Node.js",
+    ],
+    description: "An AI image generated gallery using MERN stack.",
+    thumbnail: "/projects/ai-gallery.png",
+    repo: "https://github.com/KingJulius/ai-img-gallery-frontend",
+  },
+  {
+    title: "Expense Tracker",
+    repo: "https://github.com/KingJulius/expense-tracker",
+    thumbnail: "/projects/expensetracker.png",
+    description: "A Web App Developed to keep track of expenses.",
+    tags: ["React", "JavaScript"],
+  },
+  {
+    title: "Food Dish Classifier",
+    repo: "https://github.com/KingJulius/Predict-that-Dish",
+    thumbnail: "/projects/foodclassifier.png",
+    description:
+      "Website that allows users to upload image of their dish and it returns the label along with the confidence score.",
+    tags: ["Python", "TensorFlow", "Flask", "React", "NGINX", "AWS EC2"],
+  },
+  {
+    title: "Minesweeper Java Application",
+    repo: "https://github.com/KingJulius/Minesweeper",
+    thumbnail: "/projects/minesweeper.png",
+    description:
+      "A GUI game using a client/server architecture to load and save an instance of the game and to display the top scores.",
+    tags: ["Java", "Swing", "SQLite"],
+  },
+  {
+    title: "Real-Time Digit Classification",
+    repo: "https://github.com/KingJulius/Real-Time-Digit-Classification",
+    thumbnail: "/projects/digitclassifier.png",
+    description: "Digit Classification using Real-Time Image Feed.",
+    tags: ["Python", "OpenCV", "Keras", "TensorFlow"],
+  },
+];
