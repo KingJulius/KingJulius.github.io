dada70a070f52e62cfffcdf96e993d41efc83f7b
Julius Dsouza <jsdsz1996@gmail.com>
2024-12-20 22:39:21 -0800

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output:  process.env.NODE_ENV !== "production" ? undefined: "export", 
 };
 
 export default nextConfig;
