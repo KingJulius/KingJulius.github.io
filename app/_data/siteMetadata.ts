303f3992cf638b1e130d2e2b6c99c90ea149548e
Julius Dsouza <jsdsz1996@gmail.com>
2024-12-26 13:29:03 +0400

    Added Assets

---
+++ app/_data/siteMetadata.ts
@@ -0,0 +1,13 @@
+export const siteMetadata = {
+  title: "Julius' Portfolio",
+  author: "Julius Dsouza",
+  siteUrl: "https://kingjulius.github.io/",
+  description:
+    "Julius Dsouza - Fullstack developer passionate about creating things on internet",
+  keywords: [`Julius Dsouza, Julius, jsdsouza, jsdsz`],
+  social: {
+    email: "jsdsz1996@gmail.com",
+    linkedinLink: `https://www.linkedin.com/in/julius-dsouza/`,
+    githubLink: `https://github.com/KingJulius`,
+  },
+};
---
+++ app/_data/skills.ts
@@ -0,0 +1,59 @@
+import HTML from "@/public/library-icons/html5-original.svg";
+import DJANGO from "@/public/library-icons/django-plain.svg";
+import EXPRESS from "@/public/library-icons/express-original.svg";
+import FLASK from "@/public/library-icons/flask-original.svg";
+import C from "@/public/library-icons/c-original.svg";
+import JAVA from "@/public/library-icons/java-original.svg";
+import JAVASCRIPT from "@/public/library-icons/javascript-original.svg";
+import MARKDOWN from "@/public/library-icons/markdown-original.svg";
+import MONGODB from "@/public/library-icons/mongodb-original.svg";
+import MYSQL from "@/public/library-icons/mysql-original.svg";
+import NEXTJS from "@/public/library-icons/nextjs-line.svg";
+import NODEJS from "@/public/library-icons/nodejs-original.svg";
+import PYTHON from "@/public/library-icons/python-original.svg";
+import PYTORCH from "@/public/library-icons/pytorch-original.svg";
+import REACT from "@/public/library-icons/react-original.svg";
+import REDUX from "@/public/library-icons/redux-original.svg";
+import REACT_NATIVE from "@/public/library-icons/react-native.svg";
+import SPRING from "@/public/library-icons/spring-original.svg";
+import TENSORFLOW from "@/public/library-icons/tensorflow-original.svg";
+import TYPESCRIPT from "@/public/library-icons/typescript-original.svg";
+import TAILWIND from "@/public/library-icons/tailwind-plain.svg";
+import UBUNTU from "@/public/tool-icons/ubuntu-plain.svg";
+import GIT from "@/public/tool-icons/git-original.svg";
+import VSCODE from "@/public/tool-icons/vscode-original.svg";
+import LINUX from "@/public/tool-icons/linux-original.svg";
+import DOCKER from "@/public/tool-icons/docker-original.svg";
+import AWS from "@/public/tool-icons/amazonwebservices-original.svg";
+import AZURE from "@/public/tool-icons/azure-original.svg";
+
+export const langauges = [
+  { id: 1, title: "PYTHON", img: PYTHON },
+  { id: 2, title: "JAVASCRIPT", img: JAVASCRIPT },
+  { id: 3, title: "TYPESCRIPT", img: TYPESCRIPT },
+  { id: 4, title: "REACT", img: REACT },
+  { id: 5, title: "REDUX", img: REDUX },
+  { id: 6, title: "NEXTJS", img: NEXTJS },
+  { id: 7, title: "NODEJS", img: NODEJS },
+  { id: 8, title: "EXPRESS", img: EXPRESS },
+  { id: 9, title: "DJANGO", img: DJANGO },
+  { id: 10, title: "FLASK", img: FLASK },
+  { id: 11, title: "HTML", img: HTML },
+  { id: 12, title: "REACT NATIVE", img: REACT_NATIVE },
+  { id: 13, title: "MONGODB", img: MONGODB },
+  { id: 14, title: "TENSORFLOW", img: TENSORFLOW },
+  { id: 15, title: "PYTORCH", img: PYTORCH },
+  { id: 16, title: "JAVA", img: JAVA },
+  { id: 17, title: "MARKDOWN", img: MARKDOWN },
+  { id: 18, title: "SPRING", img: SPRING },
+  { id: 19, title: "C", img: C },
+  { id: 20, title: "TAILWIND CSS", img: TAILWIND },
+  { id: 21, title: "UBUNTU", img: UBUNTU },
+  { id: 22, title: "GIT", img: GIT },
+  { id: 23, title: "VSCODE", img: VSCODE },
+  { id: 24, title: "MYSQL", img: MYSQL },
+  { id: 25, title: "LINUX", img: LINUX },
+  { id: 26, title: "DOCKER", img: DOCKER },
+  { id: 27, title: "AWS", img: AWS },
+  { id: 28, title: "AZURE", img: AZURE },
+];
