99301626b82d6a1656d7db9bae35b2dd6ac07b9f
Julius Dsouza <jsdsz1996@gmail.com>
2024-12-26 13:34:16 +0400

    Added home page with initial setup (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,69 @@
+import React from "react";
 import Image from "next/image";
+import { SOCIALS } from "@/@/_data/socials";
+import { SocialLink } from "@/@/_components/social-link";
+import { LINKS } from "@/@/_lib/constants";
+import { siteMetadata } from "@/@/_data/siteMetadata";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <React.Fragment>
+      <section className="mb-5 p-4 ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/me.png"
+          width={100}
+          height={100}
+          alt="avatar"
+          className="rounded-full cursor-pointer hover:grayscale mb-5"
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="text-2xl font-bold">{siteMetadata.author}</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="text-gray-700 dark:text-gray-300">
+          <p className="mt-4">
+            I’m a fullstack software engineer with over 4 years of experience specializing in building scalable
+            webites and mobile applications using a wide range of modern technologies and frameworks.
+          </p>
+          <p className="mt-4 mb-4">
+            Currently, I&apos;m working as a software engineer at&nbsp;
+            <a
+              href="https://www.apple.com/"
+              target="_blank"
+              className="border-b border-slate-950 dark:border-white inline-block"
+            >
+              Apple
+            </a>
+            .
+          </p>
+
+          <p className="mb-4">
+            If you&apos;d like to collaborate, please&nbsp;
+            <a
+              href={`mailto:${siteMetadata.social.email}`}
+              className="border-b border-slate-950 dark:border-white inline-block"
+            >
+              send me an email
+            </a>
+            &nbsp;or reach out on any of my social handles.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="flex space-x-4 mb-2 mt-4">
+          {SOCIALS.map((social) => (
+            <SocialLink
+              key={social.label}
+              aria-label={`Follow on ${social.label}`}
+              href={social.href}
+              icon={social.icon}
+            />
+          ))}
+        </div>
+        <p className="mt-4 border-b inline-block cursor-pointer border-slate-950 dark:border-white">
+          <a href={LINKS.RESUME} target="_blank" rel="noopener noreferrer">
+            View Resume
+          </a>
+        </p>
+      </section>
+    </React.Fragment>
   );
 }
